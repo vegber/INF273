9f34920d3c32a80f7edb664bee2487090def8f12
Vegard <Vegard.berge8@gmail.com>
2022-02-03 13:44:06 +0100

    *Added all files llol

--- implementation/driver_code.py
+++ implementation/driver_code.py
@@ -9,17 +9,19 @@ from utils_code.pdp_utils import *
 def load_correct_file(which_mode):
     return {1: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_7_Vehicle_3.txt'),
             2: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_18_Vehicle_5.txt'),
-            3: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_80_Vehicle_20.txt'),
-            4: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_130_Vehicle_40.txt'),
-            5: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_300_Vehicle_90.txt')}[which_mode]
+            3: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_35_Vehicle_7.txt'),
+            4: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_80_Vehicle_20.txt'),
+            5: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_130_Vehicle_40.txt'),
+            6: load_problem('../utils_code/pdp_utils/data/pd_problem/Call_300_Vehicle_90.txt')}[which_mode]
 
 
 def filename(file: int):
     return {1: 'Call_7_Vehicle_3.txt',
             2: 'Call_18_Vehicle_5.txt',
-            3: 'Call_80_Vehicle_20.txt',
-            4: 'Call_130_Vehicle_40.txt',
-            5: 'Call_300_Vehicle_90.txt'}[file]
+            3: 'Call_35_Vehicle_7.txt',
+            4: 'Call_80_Vehicle_20.txt',
+            5: 'Call_130_Vehicle_40.txt',
+            6: 'Call_300_Vehicle_90.txt'}[file]
 
 
 sol = [7, 7, 5, 5, 0, 2, 2, 0, 3, 4, 4, 3, 1, 1, 0, 6, 6]
@@ -99,11 +101,13 @@ def run_all(var: int):
     if var == 2:
         main_run(2, 5, 18)
     if var == 3:
-        main_run(3, 20, 80)
+        main_run(3, 7, 35)
     if var == 4:
-        main_run(4, 40, 130)
+        main_run(4, 20, 80)
     if var == 5:
-        main_run(5, 90, 300)
+        main_run(5, 40, 130)
+    if var == 6:
+        main_run(6, 90, 300)
 
 
 if __name__ == '__main__':
@@ -113,7 +117,8 @@ if __name__ == '__main__':
     print("Number of processors: ", mp.cpu_count())
     pool = mp.Pool(processes=5)
 
-    pool.map(run_all, range(1, 5))
+    pool.map(run_all, range(1, 6))
 
     stop = time.time()
     print(f"Totall time was {stop - start}")
+    pool.close()
