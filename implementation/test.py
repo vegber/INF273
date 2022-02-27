6bcee326cf6c002bd1b05bebd2ba5fcd0f791b0d
Vegard <Vegard.berge8@gmail.com>
2022-02-27 21:43:42 +0100

    added test env

--- implementation/test.py
+++ implementation/test.py
@@ -1,59 +1,31 @@
-import random
-from collections.abc import Iterable
+from itertools import groupby
+
 import numpy as np
-vehicle = 5
-call = 18
+
+from random_solution_driver import get_init
+from utils_code.pdp_utils import *
+
+path = '../utils_code/pdp_utils/data/pd_problem/Call_7_Vehicle_3.txt'
+
+problem = load_problem(path)
+init_ = get_init(problem['n_vehicles'], problem['n_calls'])
+print(init_)
+sol = np.array([7, 7, 5, 5, 0, 2, 2, 0, 3, 4, 4, 3, 1, 1, 0, 6, 6])
 
 
-def get_permutation(vehicle: int, call: int):
-    return np.random.permutation([0] * vehicle + list(range(1, call)) * 2)
+def to_list(L):
+    # [list(group) for key, group in groupby(L, lambda x: x == 0)]
+    lst_string = "".join([str(x) for x in L])
+    lst2 = lst_string.split('0')
+    lst3 = [list(y) for y in lst2]
+    lst4 = [list(map(int, z)) for z in lst3]
+    print(lst4)
 
 
-def get_permutation(vehicle: int, call: int):
-    actual_solution = []
-    solution_space = []
-    available_calls = []
-
-    available_calls.extend(range(1, call + 1))
-    cars = 3  # random.randint(0, vehicle)
-
-    for car in range(cars):
-        # find random calls for each car
-        car_choice = random.sample(available_calls, (random.randint(0, len(available_calls))))
-        # append calls to solution list
-        solution_space.append(car_choice)
-        # remove calls in use:
-        available_calls = [i for i in available_calls if i not in car_choice]
-
-    # solution space need to be doubled, and permuted
-    # then solution space need to be added together, and separated with zero
-
-    for vehicle_call in solution_space:
-        # a list of calls - could be empty
-        if len(vehicle_call) == 0:
-            actual_solution.append(0)
-        else:
-            elements = [elem for elem in vehicle_call] * 2
-            random.shuffle(elements)
-            vehicle_call_ = elements + [0]
-            actual_solution.append(vehicle_call_)
-
-    # if not, all cars is used, and we need to out-source the rest
-    if len(available_calls) != 0:
-        remaining_calls = [elem for elem in available_calls] * 2
-        random.shuffle(remaining_calls)
-        actual_solution.append(remaining_calls)
-
-    return np.array(list(map(int, flatten(actual_solution))))
+def one_insert(L, prob):
+    # L to n + 1 lists
+    list_indexed = to_list(L)
+    print(list_indexed)
 
 
-def flatten(l):
-    for el in l:
-        if isinstance(el, Iterable) and not isinstance(el, (str, bytes)):
-            yield from flatten(el)
-        else:
-            yield el
-
-
-for x in range(10):
-    print(get_permutation(vehicle, call), end="\n")
+one_insert(sol, prob=problem)
