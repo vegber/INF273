d0867dd05562b338cd6ec8e18c69b3d9c24f6977
vbe020 <vbe020@uib.no>
2022-02-27 22:28:18 +0100

    local search implementation

---
+++ implementation/Algorithms.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from random_solution_driver import get_init, get_permutation
+from utils_code.pdp_utils import *
+from operators import *
+from natsort import natsorted
+import os
+
+path = '../utils_code/pdp_utils/data/pd_problem/'
+
+
+class LocalSearch:
+    def __init__(self, problem):
+        self.unpacked_problem = load_problem(path + problem)
+        self.vehicle = self.unpacked_problem['n_vehicles']
+        self.calls = self.unpacked_problem['n_calls']
+
+    def run(self, operator):  # define which operator to work on - higher order func.
+        init = operator(get_init(self.vehicle, self.calls))
+
+        # set best solution
+        best_solution = init # cost_function(init, self.unpacked_problem)
+        best_sol_cost = cost_function(best_solution, self.unpacked_problem)
+        for x in range(10000):
+            new_sol = operator(best_solution)
+            if feasibility_check(new_sol, self.unpacked_problem) and cost_function(new_sol, self.unpacked_problem) < best_sol_cost:
+                best_solution = new_sol
+                best_sol_cost = cost_function(best_solution)
+
+        return best_solution
+
+
+if __name__ == '__main__':
+    file_list = natsorted(os.listdir(path), key=lambda y: y.lower())
+    local_hero = LocalSearch(file_list[0])
+    local_hero.run(one_insert)
---
+++ implementation/operators.py
@@ -0,0 +1,9 @@
+
+def one_insert(arr):
+    return arr
+
+def two_exchange(arr):
+    return arr
+
+def three_exchange(arr):
+    return arr
