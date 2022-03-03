0f0aec54871bcec82836af307a95096fdf4a5bc5
Vegard <Vegard.berge8@gmail.com>
2022-03-03 12:36:07 +0100

    test is one step closer

--- implementation/test.py
+++ implementation/test.py
@@ -39,36 +39,72 @@ def one_insert(L, prob):
     print(problem['VesselCargo'])
 
 
+def get_index_1d(arr, first_swap_value):
+    var = [i for i, e in enumerate(arr) if e == first_swap_value]
+    # should always be two
+    return var[0], var[1]
+
+
+def which_cars(arr, first_swap_index, second_valid_index):
+    car_index = []
+    car_count = 0
+    for x in range(len(arr)):
+        if x == first_swap_index or x == second_valid_index:
+            print("yes")
+            car_index.append(car_count)
+        if arr[x] == 0:
+            car_count += 1
+    print(car_index)
+    print(f"indexes in car {car_index[0]} and {car_index[1]}")
+    return car_index[0], car_index[1]
+
+
 def two_swap(arr, vehicle, calls, prob):
     vessel_cargo = problem['VesselCargo']
     vehicle_valid_calls = feasable_placements_with_outsorce(calls, vehicle, vessel_cargo)
-    arr_2 = to_list2(arr)
-    print(f"vehicle valid cars {vehicle_valid_calls}")
+    arr_2 = fill_2d_zero(to_list2(arr))  # can have in case I need it
 
-    first_swap = random.randint(0, calls)
+    first_swap_index = random.randint(0, len(arr) - 1)
+    first_swap_value = arr[first_swap_index]
 
-    # find random car, to swap with
-    # legal cars to swap with
-    legal_cars = [x for x in range(len(vehicle_valid_calls)) if first_swap in vehicle_valid_calls[x]]
+    # so now, this value can be swapped with all calls except zero
+    second_valid_index = random.randint(0, len(arr) - 1)
+    while second_valid_index == first_swap_index:
+        second_valid_index = random.randint(0, len(arr) - 1)
 
-    print(f"random swap was {first_swap} and legal cars is then {legal_cars} ")
+    second_swap_value = arr[second_valid_index]
+    print(first_swap_value)
+    print(second_swap_value)
+    print(f"before: {arr}")
+    first_swap_value = 0
+    second_swap_value = 0
+    if first_swap_value and second_swap_value != 0:
+        # both values are not zero, we can do swap
+        pickup_first, deliver_first = get_index_1d(arr, first_swap_value)
+        pickup_sec, deliver_sec = get_index_1d(arr, second_swap_value)
+        arr = swap(arr, pickup_first, pickup_sec)
+        arr = swap(arr, deliver_first, deliver_sec)
+        print(f"after: {arr}")
+    if (first_swap_value and second_swap_value) == 0:
+        # todo
+        # swap cars
+        # find out car indexes, swap() --- use 2d arr
+        car1, car2 = which_cars(arr, first_swap_index, second_valid_index)
+        pass
+    else:  # atleast one is zero
 
-    # where is first swap:
-    first_swap_indexes = [i for i, e in enumerate(arr) if e == first_swap]
-    print(first_swap_indexes)
-
-    # pick random valid place to swap with
-    # create list of all available swaps in those cars
-    cars = [0]  # can always swap with zero
-    available_swaps = [cars.append(car) for x in legal_cars for car in arr_2[x] if car not in cars]
-    print(cars)
+        pass
     return arr
+
+    # 0 0 0 0 0 0 0 0 1,1 0 , 2,2,
 
 
 def fill_2d_zero(two_dim):
     for x in range(len(two_dim)):
         if not two_dim[x]:
             two_dim[x].append(0)
+
+    return two_dim
 
 
 def feasable_placements_with_outsorce(calls, vehicle, vessel_cargo):
